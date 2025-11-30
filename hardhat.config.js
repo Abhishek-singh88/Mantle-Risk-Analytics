a03a9dae17212e07fc7570dae836ea84d10a0faa
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-11-30 22:10:20 +0530

    adding mantle network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+const { MANTLE_PRIVATE_KEY, MANTLE_RPC_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {},
+    mantleSepolia: {
+      url: MANTLE_RPC_URL || "https://rpc.sepolia.mantle.xyz",
+      chainId: 5003,
+      accounts: MANTLE_PRIVATE_KEY ? [MANTLE_PRIVATE_KEY] : [],
+    },
+  },
 };
